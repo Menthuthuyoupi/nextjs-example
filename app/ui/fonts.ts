a4bb5014d2efc6751aabd408214b9d67bcecee3a
nicolas morales <menthuthuyoupi.nmp@gmail.com>
2024-09-01 00:54:38 -0400

    subiando nextjs dashboard cap 6

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Montserrat, Sevillana, Inter,Lusitana } from 'next/font/google'
+
+export const montserrat = Montserrat({subsets:['latin'], weight:['400','700']})
+
+export const sevillana = Sevillana({subsets:['latin'], weight:['400']})
+ 
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({subsets:['latin'] ,weight:['400']})
